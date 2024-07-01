df73f4d5dc290f5c70143604276da7869873a18d
Varun Kamat <67357457+varunthepianoman@users.noreply.github.com>
2024-07-01 14:10:37 -0700

    Initial commit: Made first ppm image

---
+++ RayTracerInOneWeekend.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Varun Kamat on 3/15/24.
+//
+#include <iostream>
+
+int main() {
+//    std::cout << "Hello, World!" << std::endl;
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            double r = double(i) / (image_width - 1);
+            double g = double(j) / (image_height - 1);
+            double b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    return 0;
+}
